59c0fcda014e35cdc40883c66885f025191452ff
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-02-04 13:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+	//your JS code here. If required.
+	const value2 =document .getElementById("text");
+	const value1=document.getElementById("delay");
+	const button=document.getElementById("btn");
+   const  ouput=document.getElementById("output");
+
+
+	async function getData(){
+	try{
+		const text=value2.value;
+		const delay = Number(value1.value);
+		output.textContent = "";
+		try {
+    await wait(delay);
+    output.textContent = text;
+  } catch (error) {
+    console.error(error);
+	}}button.addEventListener("click", getData);
