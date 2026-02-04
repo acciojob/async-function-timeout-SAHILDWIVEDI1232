357a6b3126f03f116965ce4fe369f5bc7bc01597
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-02-04 13:05:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-	//your JS code here. If required.
-	const value2 =document .getElementById("text");
-	const value1=document.getElementById("delay");
-	const button=document.getElementById("btn");
-   const  ouput=document.getElementById("output");
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
 
+// helper function to create delay
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-	async function getData(){
-	try{
-		const text=value2.value;
-		const delay = Number(value1.value);
-		output.textContent = "";
-		try {
+async function getData() {
+  const text = textInput.value;
+  const delay = Number(delayInput.value);
+
+  output.textContent = ""; // clear previous output
+
+  try {
     await wait(delay);
     output.textContent = text;
   } catch (error) {
     console.error(error);
-	}}button.addEventListener("click", getData);
+  }
+}
+
+
+button.addEventListener("click", getData);
